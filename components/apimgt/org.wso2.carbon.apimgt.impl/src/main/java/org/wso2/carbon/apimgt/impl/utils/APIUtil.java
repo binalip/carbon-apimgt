2bbb195310174ed60c36dd3712be46ed8f8a8ead
binalip91 <binalip91@gmail.com>
2015-02-26 09:46:00 +0530

    remove derecr reference to the class 'WSO2APIStore' in APIManager code

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
@@ -1359,6 +1359,12 @@ public final class APIUtil {
                     String type=storeElem.getAttributeValue(new QName(APIConstants.EXTERNAL_API_STORE_TYPE));
                     store.setType(type); //Set Store type [eg:wso2]
                     String name=storeElem.getAttributeValue(new QName(APIConstants.EXTERNAL_API_STORE_ID));
+                    String className=storeElem.getAttributeValue(new QName(APIConstants.EXTERNAL_API_STORE_CLASS_NAME));
+
+                    Class publisherClass = APIStore.class.getClassLoader().loadClass(className);
+                    APIPublisher publisher = (APIPublisher)publisherClass.newInstance();
+                    store.setPublisher(publisher);
+
                     if(name==null){
                         try {
                             throw new APIManagementException("The ExternalAPIStore name attribute is not defined in api-manager.xml.");
@@ -1406,6 +1412,15 @@ public final class APIUtil {
             String msg = "Malformed XML found in the External Stores Configuration resource";
             log.error(msg, e);
             throw new APIManagementException(msg, e);
+        } catch (ClassNotFoundException e) {
+            String msg = "Unable to find class";
+            log.error(msg, e);
+        } catch (InstantiationException e) {
+            String msg = "Unable to instantiate class";
+            log.error(msg, e);
+        } catch (IllegalAccessException e) {
+            String msg = "Illegal attempt to invoke class methods";
+            log.error(msg, e);
         }
         return externalAPIStores;
     }
@@ -2103,7 +2118,6 @@ public final class APIUtil {
 	/**
 	 * Load the External API Store Configuration  to the registry
 	 * 
-	 * @param tenant
 	 * @param tenantID
 	 * @throws org.wso2.carbon.apimgt.api.APIManagementException
 	 */
